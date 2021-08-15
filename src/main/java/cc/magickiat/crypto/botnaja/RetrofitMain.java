c68d72bef22ba3c87b4e9c95b720007a11832513
winweb <theerasun@gmail.com>
2021-08-15 14:55:35 +0700

    move balance info logic to service class.

--- src/main/java/cc/magickiat/crypto/botnaja/RetrofitMain.java
+++ src/main/java/cc/magickiat/crypto/botnaja/RetrofitMain.java
@@ -5,10 +5,7 @@ import cc.magickiat.crypto.botnaja.dto.BalanceInfo;
 import cc.magickiat.crypto.botnaja.dto.Ticker;
 import cc.magickiat.crypto.botnaja.service.BitKubService;
 
-import java.math.BigDecimal;
 import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 public class RetrofitMain {
 
@@ -33,26 +30,7 @@ public class RetrofitMain {
         System.out.println("===== Ticker for THB_BTC =====");
         System.out.println(tickerMap);
 
-        Map<String, BalanceInfo> balanceInfoMap = balances.entrySet()
-                .stream()
-                .filter(b -> b.getValue().getAvailable().compareTo(BigDecimal.ZERO) != 0 || b.getValue().getReserved().compareTo(BigDecimal.ZERO) != 0)
-                .collect(Collectors.toMap(
-                        t -> t.getKey(),
-                        e -> {
-                            final BigDecimal available = e.getValue().getAvailable();
-                            final BigDecimal reserved = e.getValue().getReserved();
-                            final Ticker ticker = "THB".equals(e.getKey())? null: finalTickerMap.get("THB_" + e.getKey());
-
-                            return BalanceInfo.builder()
-                                    .available(available)
-                                    .reserved(reserved)
-                                    .ticker(ticker == null? new Ticker(): ticker)
-                                    .value(ticker == null? null: available.add(reserved).multiply(ticker.getLast()))
-                                    .build();
-                        }));
-
-        Map<String, BalanceInfo> sortBalanceInfoMap = new TreeMap<>(balanceInfoMap);
-
+        Map<String, BalanceInfo> sortBalanceInfoMap = service.getInfoMap(balances, finalTickerMap);
         System.out.println("\n===== My Balances Info =====");
         sortBalanceInfoMap.forEach((k, v) -> System.out.printf("%-6s = %s%n", k, v));
 
--- src/main/java/cc/magickiat/crypto/botnaja/service/BitKubService.java
+++ src/main/java/cc/magickiat/crypto/botnaja/service/BitKubService.java
@@ -1,6 +1,7 @@
 package cc.magickiat.crypto.botnaja.service;
 
 import cc.magickiat.crypto.botnaja.dto.Balance;
+import cc.magickiat.crypto.botnaja.dto.BalanceInfo;
 import cc.magickiat.crypto.botnaja.dto.BitKubRequestBody;
 import cc.magickiat.crypto.botnaja.dto.Ticker;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -12,7 +13,11 @@ import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
@@ -68,7 +73,11 @@ public class BitKubService {
         BitKubRequestBody requestBody = new BitKubRequestBody();
         requestBody.setTs(ts);
         requestBody.setSig(HmacService.calculateHmac("{\"ts\":" + ts + "}"));
-        return api.getBalances(requestBody).execute().body().getResult();
+
+        var result = api.getBalances(requestBody).execute().body();
+
+        assert result != null;
+        return result.getResult();
     }
 
     public Map<String, Ticker> getTickers() throws IOException {
@@ -82,15 +91,48 @@ public class BitKubService {
         BitKubApi api = retrofit.create(BitKubApi.class);
         return api.marketTickers(symbol).execute().body();
     }
+
+    public Map<String, BalanceInfo> getInfoMap() throws IOException {
+        return getInfoMap(null, null);
+    }
+
+    public Map<String, BalanceInfo> getInfoMap(Map<String, Balance> balances, Map<String, Ticker> tickerMap) throws IOException {
+        if(balances == null) {
+            balances = getBalances();
+        }
+
+        if(tickerMap == null) {
+            tickerMap = getTickers();
+        }
+
+        Map<String, Ticker> finalTickerMap = tickerMap;
+
+        Map<String, BalanceInfo> balanceInfoMap = balances.entrySet()
+                .stream()
+                .filter(b -> b.getValue().getAvailable().compareTo(BigDecimal.ZERO) != 0 || b.getValue().getReserved().compareTo(BigDecimal.ZERO) != 0)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> {
+                            final BigDecimal available = e.getValue().getAvailable();
+                            final BigDecimal reserved = e.getValue().getReserved();
+                            final Ticker ticker = "THB".equals(e.getKey())? null: finalTickerMap.get("THB_" + e.getKey());
+
+                            return BalanceInfo.builder()
+                                    .available(available)
+                                    .reserved(reserved)
+                                    .ticker(ticker == null? new Ticker(): ticker)
+                                    .value(ticker == null? null: available.add(reserved).multiply(ticker.getLast()))
+                                    .build();
+                        }));
+
+        return new TreeMap<>(balanceInfoMap);
+    }
 }
 
 class HttpClient {
     private static final Logger log = LogManager.getLogger(HttpClient.class);
 
-    private static final HttpLoggingInterceptor loggingInterceptor =
-            new HttpLoggingInterceptor((msg) -> {
-                log.debug(msg);
-            });
+    private static final HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(log::debug);
 
     static {
         if (log.isTraceEnabled()) {
