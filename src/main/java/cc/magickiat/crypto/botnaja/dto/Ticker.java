f097cfdff0220b048b3a92601f3338107384beeb
winweb <theerasun@gmail.com>
2021-08-16 12:20:44 +0700

    add more fieldsn

--- src/main/java/cc/magickiat/crypto/botnaja/dto/Ticker.java
+++ src/main/java/cc/magickiat/crypto/botnaja/dto/Ticker.java
@@ -1,16 +1,28 @@
 package cc.magickiat.crypto.botnaja.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
 
+@SuperBuilder
+@NoArgsConstructor
 @Data
 @ToString
 public class Ticker {
 
-    private int id;
-    private BigDecimal last;
-    private BigDecimal percentChange;
+    protected int id;
+    protected BigDecimal last;
+    protected BigDecimal lowestAsk;
+    protected BigDecimal highestBid;
+    protected BigDecimal change;
+    protected BigDecimal percentChange;
+    protected BigDecimal baseVolume;
+    protected BigDecimal quoteVolume;
+    protected int isFrozen;
+    protected BigDecimal high24hr;
+    protected BigDecimal low24hr;
 
 }
