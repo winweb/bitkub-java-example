142d7406e4491d4759738a8b3bd5e6d3342cb0cf
winweb <theerasun@gmail.com>
2021-08-21 20:47:27 +0700

    tune performance.

--- src/main/java/cc/magickiat/crypto/botnaja/service/HmacService.java
+++ src/main/java/cc/magickiat/crypto/botnaja/service/HmacService.java
@@ -5,8 +5,12 @@ import org.apache.commons.codec.digest.HmacUtils;
 
 public class HmacService {
     private static final String BITKUB_API_SECRET = System.getenv("BITKUB_API_SECRET");
+    private static final HmacUtils hmacUtils = new HmacUtils(HmacAlgorithms.HMAC_SHA_256, BITKUB_API_SECRET);
+
+    public HmacService() {
+    }
 
     public static String calculateHmac(String data) {
-        return new HmacUtils(HmacAlgorithms.HMAC_SHA_256, BITKUB_API_SECRET).hmacHex(data);
+        return hmacUtils.hmacHex(data);
     }
 }
