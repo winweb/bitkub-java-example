5c36dffcac594c9606d0b6f2797fa445a236f24a
winweb <theerasun@gmail.com>
2021-10-06 21:36:34 +0700

    Update Ticker.java
    use  @JsonIgnore.

--- src/main/java/cc/magickiat/crypto/botnaja/dto/Ticker.java
+++ src/main/java/cc/magickiat/crypto/botnaja/dto/Ticker.java
@@ -1,5 +1,6 @@
 package cc.magickiat.crypto.botnaja.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -13,14 +14,18 @@ import java.math.BigDecimal;
 @ToString
 public class Ticker {
 
+    @JsonIgnore
     protected int id;
     protected BigDecimal last;
+    @JsonIgnore
     protected BigDecimal lowestAsk;
+    @JsonIgnore
     protected BigDecimal highestBid;
     protected BigDecimal change;
     protected BigDecimal percentChange;
     protected BigDecimal baseVolume;
     protected BigDecimal quoteVolume;
+    @JsonIgnore
     protected int isFrozen;
     protected BigDecimal high24hr;
     protected BigDecimal low24hr;
